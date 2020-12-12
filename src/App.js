c7810b1f122745dd312ad3d199059fdb8ec1b3fa
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-12-12 18:33:49 +0100

    Add SEO

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 
 const App = () => {
+
   return (
     <div className="antialiased text-gray-900 bg-gray-200 min-h-screen">
       <Header />
