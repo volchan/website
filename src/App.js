15782019b8530d01c19b6eac7c8daa7989bd452b
stephane de la torre <s.dlt@hotmail.fr>
2020-12-23 00:50:11 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="antialiased text-gray-900 bg-gray-200 min-h-screen">
       <Navbar />
       <Header />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+const Footer = () => {
+  return (
+    <div className="h-10 bg-gray-900 flex items-center px-20 font-medium">
+      <p className="m-0 text-gray-500 tracking-tight">
+        Dévellopé avec <FontAwesomeIcon icon={faHeart} className="text-indigo-500" /> par
+      </p>
+      &nbsp;
+      <a
+        href="https://www.github.com/volchan"
+        target="_blank noreferrer"
+        rel="noopener noreferrer"
+        className="text-indigo-500 hover:text-indigo-600"
+      >
+        Stéphane DE LA TORRE
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
