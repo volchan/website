019e8bd436a1c15a9bc1261a0ba6100ab7b26c0c
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2021-01-01 15:25:28 +0100

    Rework Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 import HeaderBackground from '../assets/images/header_background.png';
 
 const Header = () => {
   return (
     <header
-      className="relative flex flex-col items-center justify-center h-screen w-full bg-cover bg-center text-gray-100"
+      className="relative flex flex-col items-center justify-center h-screen w-full bg-cover bg-center text-gray-100 text-shadow-header"
       style={{ backgroundImage: `url(${HeaderBackground})` }}
       id="home"
     >
-      <h1 className="font-bold tracking-tight text-shadow-md text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
+      <h1 className="font-bold tracking-tight text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
         Stéphane DE LA TORRE
       </h1>
-      <p className="mt-7 text-shadow-sm text-gray-500 font-semibold px-4 text-center text-xs lg:text-sm xl:text-lg">
+      <p className="mt-7 text-gray-500 font-semibold px-4 text-center text-xs lg:text-sm xl:text-lg">
         Développeur Fullstack Ruby on Rails basé à Bordeaux le jour,
         <br className="block md:hidden" /> passionné touche à tout la nuit.
       </p>
@@ -23,7 +24,7 @@ const Header = () => {
           href="https://www.linkedin.com/in/st%C3%A9phane-de-la-torre-b748a3144/"
           target="_blank"
           rel="noopener noreferrer"
-          className=" hover:text-indigo-500 text-shadow-md md:text-sm lg:text-base"
+          className=" hover:text-indigo-500 text-xl lg:text-2xl"
         >
           <FontAwesomeIcon icon={faLinkedinIn} />
         </a>
@@ -31,11 +32,13 @@ const Header = () => {
           href="https://github.com/volchan"
           target="_blank"
           rel="noopener noreferrer"
-          className="ml-2 hover:text-indigo-500 text-shadow-md md:text-sm lg:text-base"
+          className="ml-4 hover:text-indigo-500 text-xl lg:text-2xl"
         >
           <FontAwesomeIcon icon={faGithub} />
         </a>
       </div>
+
+      <FontAwesomeIcon icon={faChevronDown} className="text-xl absolute bottom-16 sm:bottom-10 mx-auto motion-safe:animate-bounce" />
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
       colors: {
         body: '#070610',
       },
+      textShadow: {
+        'header': '0px 1px 3px rgba(0, 0, 0, .8)',
+     }
     },
   },
   variants: {
     extend: {
+      animation: ['motion-safe'],
       padding: ['last'],
     },
   },
